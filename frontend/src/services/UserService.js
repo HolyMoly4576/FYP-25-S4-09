76f4c008d217a03af389db42a839a9b1f073c40a
Proatrotting <graceteyyutong@gmail.com>
2025-12-15 15:37:23 +0800

    Update API_BASE_URL to REACT

--- frontend/src/services/UserService.js
+++ frontend/src/services/UserService.js
@@ -1,4 +1,4 @@
-export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;;  
+export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
 const TOKEN_KEY = "accessToken";
 const USER_KEY = "user";
 
